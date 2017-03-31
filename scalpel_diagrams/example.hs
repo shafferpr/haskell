0868867d0ac0dc43c8a8128a59df8b00b193c82f
shafferpr <shafferpr@gmail.com>
2017-03-30 21:46:44 -0400

    commiting example.hs because it compiles and supposedly creates a map but haven't tested yet

--- scalpel_diagrams/example.hs
+++ scalpel_diagrams/example.hs
@@ -9,6 +9,7 @@ import Diagrams.TwoD.Arrow
 import Text.HTML.Scalpel as Scalpel
 import Control.Applicative
 import Data.Monoid
+import Control.Monad
 import Data.Maybe
 import qualified Data.Set as Set
 import qualified Data.Map as Map
@@ -44,23 +45,52 @@ tournament xs = atPoints (trailVertices $ regPoly (length xs) 1) (map node xs)
 example :: [(String,Float)] -> Diagram B
 example xs = tournament xs
 
+getLines :: FilePath -> IO [String]
+getLines = liftM lines . readFile
 
 main :: IO ()
 main = do
-  xs <- sequence $ map (\x -> takeComments <$> (allComments x)) [13]
+  commonWords <- getLines "1000.txt"
+  let commonWordsSet = Set.fromList commonWords
+  --mapM_ putStrLn commonWords
+  xs <- sequence $ map (\x -> takeComments <$> (allComments x)) [13,14]
   --xs <- sequence $ map (\x -> fmap takeComments (fmap fromJust $ allComments x)) [12..13]
   --let set1 = Set.fromList $ concat $ map words $ map concat xs
   let fullList = listOfWords xs
   let set1 = Set.fromList $ fullList
-  let map1 = Map.fromListWith (+) (zip fullList [0.1,0.1..])
-  mainWith $ example $ take 6 $ sortListBySecondElement (Map.toList map1)
+  --let uniqueWords = Set.difference set1 $ commonWordsSet
+  let reducedList = nub $ filterCommonWords fullList commonWordsSet
+  let zeroMap = createZeroMap reducedList
+  let ys = map listOfWordsInPost xs
+  let zs = map (\x -> filterCommonWords x commonWordsSet ) ys
+  --let mapOfWords = createMapOfWords zs zeroMap
+  let map1 = Map.fromListWith (+) (zip reducedList [0.1,0.1..])
+  mainWith $ example $ take 8 $ sortListBySecondElement (Map.toList map1)
   --mainWith $ example $ take 5 $ Set.elems set1
+
+createZeroMap :: [String] -> Map.Map String (Map.Map String Float)
+createZeroMap xs = Map.fromList (zip xs (cycle [simpleList]) )
+  where simpleList = Map.fromList (zip xs [0,0..])
+
+createMapOfWords :: [[String]] -> Map.Map String (Map.Map String Float) -> Map.Map String (Map.Map String Float)
+createMapOfWords xs emptyMap = foldl (foldingFunction) emptyMap xs
+
+foldingFunction :: Map.Map String (Map.Map String Float) -> [String] -> Map.Map String (Map.Map String Float)
+foldingFunction xm xs =  foldl (\acc (a,b) -> Map.adjust (\q -> Map.adjust (+0.1) a q) b acc) xm [(x,y) | x <- xs, y <- xs]
+
+--Map.adjust (\x -> Map.adjust (+1) "cat" x) "dog" xmap
 
 sortListBySecondElement :: (Ord b) => [(a,b)] -> [(a,b)]
 sortListBySecondElement xs = reverse $ sortBy (compare `on` snd) xs
 
 listOfWords :: [Maybe [String]] -> [String]
 listOfWords xs = words $ concat $ concat $ concat <$> xs
+
+filterCommonWords :: [String] -> Set.Set String -> [String]
+filterCommonWords xs commonWordsSet = filter (\x -> Set.notMember x commonWordsSet) xs
+
+listOfWordsInPost :: Maybe [String] -> [String]
+listOfWordsInPost xs = words $ concat $ concat <$> xs
 
 allComments :: Int -> IO (Maybe [Comment]) --returns an IO action with a type of Maybe [Comment]
 allComments a = scrapeURL ("https://www.biostars.org/p/" ++ show a ++ "/") comments
